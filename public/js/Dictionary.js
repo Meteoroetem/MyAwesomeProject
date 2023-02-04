b901b695e3f348b6e5a2d3666dcb9b047a83e404
Meteoroetem <114942884+Meteoroetem@users.noreply.github.com>
2023-02-04 16:13:56 +0200

    Rearrangement of files

--- public/js/Dictionary.js
+++ public/js/Dictionary.js
@@ -67,33 +67,3 @@ function Translate(letter){
             return ',';
     }
 }
-
-
-function ButtonPress(){
-    var msg = document.getElementById("form1").value;
-    msgArr = Array.from(msg)
-    for(let i = 0; i < msgArr.length; i++){
-        msgArr[i] = Translate(msgArr[i])
-    }
-    //alert(msgArr.join(''));
-    document.getElementById("Result").innerText = msgArr.join('')
-}
-
-var pTextBoxValue = "";
-var textBoxLength = 0;
-var textBoxValue = ""
-
-function ChangeLength(){
-    textBoxValue = document.getElementById("form1").value;
-    textBoxLength = parseInt(document.getElementById("form1").getAttribute("size"));
-        if(textBoxValue.split('').length > pTextBoxValue.split('').length){
-            document.getElementById("form1").setAttribute("size", textBoxLength + 1);
-        }
-        else
-        {
-            document.getElementById("form1").setAttribute("size", textBoxLength - 1);
-        }
-        pTextBoxValue = document.getElementById("form1").value;
-    }
-
-    
